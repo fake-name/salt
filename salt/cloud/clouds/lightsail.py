2ac4ee28a31c4323a0252f99cdc24c0ded8db6db
Fake-Name <something@fake-url.com>
2017-08-13 22:11:18 -0700

    I have no idea what I'm doing.

--- salt/cloud/clouds/lightsail.py
+++ salt/cloud/clouds/lightsail.py
@@ -49,9 +49,9 @@ try:
     from botocore.exceptions import ClientError
     logging.getLogger('boto3').setLevel(logging.DEBUG)
     # logging.getLogger('boto3').setLevel(logging.CRITICAL)
-    HAS_BOTO = True
+    HAS_BOTO3 = True
 except ImportError:
-    HAS_BOTO = False
+    HAS_BOTO3 = False
 
 
 
@@ -89,9 +89,15 @@ def get_dependencies():
     '''
     return config.check_driver_dependencies(
         __virtualname__,
-        {'boto3': HAS_BOTO}
+        {'boto3': HAS_BOTO3}
     )
 
+
+def __init__(opts):
+    salt.utils.compat.pack_dunder(__name__)
+    if HAS_BOTO3:
+        __utils__['boto3.assign_funcs'](
+            __name__, 'lightsail')
 
 
 # def _cache_provider_details(conn=None):
@@ -123,7 +129,7 @@ def avail_locations(conn=None):
     '''
     return available datacenter locations
     '''
-    return _query('regions/list')
+    return _query({"includeAvailabilityZones": True})
 
 
 def avail_sizes(conn=None):
@@ -138,6 +144,41 @@ def avail_images(conn=None):
     Return available images
     '''
     return _query('os/list')
+
+
+def get_location(vm_=None):
+    '''
+    Return the EC2 region to use, in this order:
+        - CLI parameter
+        - VM parameter
+        - Cloud profile setting
+    '''
+    return __opts__.get(
+        'location',
+        config.get_cloud_config_value(
+            'location',
+            vm_ or get_configured_provider(),
+            __opts__,
+            default=DEFAULT_LOCATION,
+            search_global=False
+        )
+    )
+
+
+def get_provider(vm_=None):
+    '''
+    Extract the provider name from vm
+    '''
+    if vm_ is None:
+        provider = __active_provider_name__ or 'lightsail'
+    else:
+        provider = vm_.get('provider', 'lightsail')
+
+    if ':' in provider:
+        prov_comps = provider.split(':')
+        provider = prov_comps[0]
+    return provider
+
 
 
 def list_nodes(**kwargs):
@@ -242,242 +283,223 @@ def list_nodes(**kwargs):
 #         return False
 
 
-# def create(vm_):
-#     '''
-#     Create a single VM from a data dict
-#     '''
-#     if 'driver' not in vm_:
-#         vm_['driver'] = vm_['provider']
-
-#     private_networking = config.get_cloud_config_value(
-#         'enable_private_network', vm_, __opts__, search_global=False, default=False,
-#     )
-
-#     if private_networking is not None:
-#         if not isinstance(private_networking, bool):
-#             raise SaltCloudConfigError("'private_networking' should be a boolean value.")
-#     if private_networking is True:
-#         enable_private_network = 'yes'
-#     else:
-#         enable_private_network = 'no'
-
-#     __utils__['cloud.fire_event'](
-#         'event',
-#         'starting create',
-#         'salt/cloud/{0}/creating'.format(vm_['name']),
-#         args=__utils__['cloud.filter_event']('creating', vm_, ['name', 'profile', 'provider', 'driver']),
-#         sock_dir=__opts__['sock_dir'],
-#         transport=__opts__['transport']
-#     )
-
-#     osid = _lookup_vultrid(vm_['image'], 'avail_images', 'OSID')
-#     if not osid:
-#         log.error('Vultr does not have an image with id or name {0}'.format(vm_['image']))
-#         return False
-
-#     vpsplanid = _lookup_vultrid(vm_['size'], 'avail_sizes', 'VPSPLANID')
-#     if not vpsplanid:
-#         log.error('Vultr does not have a size with id or name {0}'.format(vm_['size']))
-#         return False
-
-#     dcid = _lookup_vultrid(vm_['location'], 'avail_locations', 'DCID')
-#     if not dcid:
-#         log.error('Vultr does not have a location with id or name {0}'.format(vm_['location']))
-#         return False
-
-#     kwargs = {
-#         'label': vm_['name'],
-#         'OSID': osid,
-#         'VPSPLANID': vpsplanid,
-#         'DCID': dcid,
-#         'hostname': vm_['name'],
-#         'enable_private_network': enable_private_network,
-#     }
-
-#     log.info('Creating Cloud VM {0}'.format(vm_['name']))
-
-#     __utils__['cloud.fire_event'](
-#         'event',
-#         'requesting instance',
-#         'salt/cloud/{0}/requesting'.format(vm_['name']),
-#         args={
-#             'kwargs': __utils__['cloud.filter_event']('requesting', kwargs, list(kwargs)),
-#         },
-#         sock_dir=__opts__['sock_dir'],
-#         transport=__opts__['transport'],
-#     )
-
-#     try:
-#         data = _query('server/create', method='POST', data=_urlencode(kwargs))
-#         if int(data.get('status', '200')) >= 300:
-#             log.error('Error creating {0} on Vultr\n\n'
-#                 'Vultr API returned {1}\n'.format(vm_['name'], data))
-#             log.error('Status 412 may mean that you are requesting an\n'
-#                       'invalid location, image, or size.')
-
-#             __utils__['cloud.fire_event'](
-#                 'event',
-#                 'instance request failed',
-#                 'salt/cloud/{0}/requesting/failed'.format(vm_['name']),
-#                 args={'kwargs': kwargs},
-#                 sock_dir=__opts__['sock_dir'],
-#                 transport=__opts__['transport'],
-#             )
-#             return False
-#     except Exception as exc:
-#         log.error(
-#             'Error creating {0} on Vultr\n\n'
-#             'The following exception was thrown when trying to '
-#             'run the initial deployment: \n{1}'.format(
-#                 vm_['name'], str(exc)
-#             ),
-#             # Show the traceback if the debug logging level is enabled
-#             exc_info_on_loglevel=logging.DEBUG
-#         )
-#         __utils__['cloud.fire_event'](
-#             'event',
-#             'instance request failed',
-#             'salt/cloud/{0}/requesting/failed'.format(vm_['name']),
-#             args={'kwargs': kwargs},
-#             sock_dir=__opts__['sock_dir'],
-#             transport=__opts__['transport'],
-#         )
-#         return False
-
-#     def wait_for_hostname():
-#         '''
-#         Wait for the IP address to become available
-#         '''
-#         data = show_instance(vm_['name'], call='action')
-#         main_ip = str(data.get('main_ip', '0'))
-#         if main_ip.startswith('0'):
-#             time.sleep(3)
-#             return False
-#         return data['main_ip']
-
-#     def wait_for_default_password():
-#         '''
-#         Wait for the IP address to become available
-#         '''
-#         data = show_instance(vm_['name'], call='action')
-#         # print("Waiting for default password")
-#         # pprint.pprint(data)
-#         if str(data.get('default_password', '')) == '':
-#             time.sleep(1)
-#             return False
-#         return data['default_password']
-
-#     def wait_for_status():
-#         '''
-#         Wait for the IP address to become available
-#         '''
-#         data = show_instance(vm_['name'], call='action')
-#         # print("Waiting for status normal")
-#         # pprint.pprint(data)
-#         if str(data.get('status', '')) != 'active':
-#             time.sleep(1)
-#             return False
-#         return data['default_password']
-
-#     def wait_for_server_state():
-#         '''
-#         Wait for the IP address to become available
-#         '''
-#         data = show_instance(vm_['name'], call='action')
-#         # print("Waiting for server state ok")
-#         # pprint.pprint(data)
-#         if str(data.get('server_state', '')) != 'ok':
-#             time.sleep(1)
-#             return False
-#         return data['default_password']
-
-#     vm_['ssh_host'] = __utils__['cloud.wait_for_fun'](
-#         wait_for_hostname,
-#         timeout=config.get_cloud_config_value(
-#             'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
-#     )
-#     vm_['password'] = __utils__['cloud.wait_for_fun'](
-#         wait_for_default_password,
-#         timeout=config.get_cloud_config_value(
-#             'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
-#     )
-#     __utils__['cloud.wait_for_fun'](
-#         wait_for_status,
-#         timeout=config.get_cloud_config_value(
-#             'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
-#     )
-#     __utils__['cloud.wait_for_fun'](
-#         wait_for_server_state,
-#         timeout=config.get_cloud_config_value(
-#             'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
-#     )
-
-#     __opts__['hard_timeout'] = config.get_cloud_config_value(
-#         'hard_timeout',
-#         get_configured_provider(),
-#         __opts__,
-#         search_global=False,
-#         default=None,
-#     )
-
-#     # Bootstrap
-#     ret = __utils__['cloud.bootstrap'](vm_, __opts__)
-
-#     ret.update(show_instance(vm_['name'], call='action'))
-
-#     log.info('Created Cloud VM \'{0[name]}\''.format(vm_))
-#     log.debug(
-#         '\'{0[name]}\' VM creation details:\n{1}'.format(
-#         vm_, pprint.pformat(data)
-#             )
-#     )
-
-#     __utils__['cloud.fire_event'](
-#         'event',
-#         'created instance',
-#         'salt/cloud/{0}/created'.format(vm_['name']),
-#         args=__utils__['cloud.filter_event']('created', vm_, ['name', 'profile', 'provider', 'driver']),
-#         sock_dir=__opts__['sock_dir'],
-#         transport=__opts__['transport']
-#     )
-
-#     return ret
-
-def get_location(vm_=None):
+def create(vm_):
     '''
-    Return the EC2 region to use, in this order:
-        - CLI parameter
-        - VM parameter
-        - Cloud profile setting
+    Create a single VM from a data dict
     '''
-    return __opts__.get(
-        'location',
-        config.get_cloud_config_value(
-            'location',
-            vm_ or get_configured_provider(),
-            __opts__,
-            default=DEFAULT_LOCATION,
-            search_global=False
-        )
-    )
 
+    # Needed query params:
+    # {
+    #    "availabilityZone": "string",
+    #    "blueprintId": "string",
+    #    "bundleId": "string",
+    #    "customImageName": "string",
+    #    "instanceNames": [ "string" ],
+    #    "keyPairName": "string",
+    #    "userData": "string"
+    # }
 
-def get_provider(vm_=None):
-    '''
-    Extract the provider name from vm
-    '''
-    if vm_ is None:
-        provider = __active_provider_name__ or 'lightsail'
-    else:
-        provider = vm_.get('provider', 'lightsail')
+    pass
 
-    if ':' in provider:
-        prov_comps = provider.split(':')
-        provider = prov_comps[0]
-    return provider
+    # private_networking = config.get_cloud_config_value(
+    #     'enable_private_network', vm_, __opts__, search_global=False, default=False,
+    # )
 
+    # if private_networking is not None:
+    #     if not isinstance(private_networking, bool):
+    #         raise SaltCloudConfigError("'private_networking' should be a boolean value.")
+    # if private_networking is True:
+    #     enable_private_network = 'yes'
+    # else:
+    #     enable_private_network = 'no'
 
-def _query(path, method='GET', data=None, params=None, header_dict=None, decode=True):
+    # __utils__['cloud.fire_event'](
+    #     'event',
+    #     'starting create',
+    #     'salt/cloud/{0}/creating'.format(vm_['name']),
+    #     args=__utils__['cloud.filter_event']('creating', vm_, ['name', 'profile', 'provider', 'driver']),
+    #     sock_dir=__opts__['sock_dir'],
+    #     transport=__opts__['transport']
+    # )
+
+    # osid = _lookup_vultrid(vm_['image'], 'avail_images', 'OSID')
+    # if not osid:
+    #     log.error('Vultr does not have an image with id or name {0}'.format(vm_['image']))
+    #     return False
+
+    # vpsplanid = _lookup_vultrid(vm_['size'], 'avail_sizes', 'VPSPLANID')
+    # if not vpsplanid:
+    #     log.error('Vultr does not have a size with id or name {0}'.format(vm_['size']))
+    #     return False
+
+    # dcid = _lookup_vultrid(vm_['location'], 'avail_locations', 'DCID')
+    # if not dcid:
+    #     log.error('Vultr does not have a location with id or name {0}'.format(vm_['location']))
+    #     return False
+
+    # kwargs = {
+    #     'label': vm_['name'],
+    #     'OSID': osid,
+    #     'VPSPLANID': vpsplanid,
+    #     'DCID': dcid,
+    #     'hostname': vm_['name'],
+    #     'enable_private_network': enable_private_network,
+    # }
+
+    # log.info('Creating Cloud VM {0}'.format(vm_['name']))
+
+    # __utils__['cloud.fire_event'](
+    #     'event',
+    #     'requesting instance',
+    #     'salt/cloud/{0}/requesting'.format(vm_['name']),
+    #     args={
+    #         'kwargs': __utils__['cloud.filter_event']('requesting', kwargs, list(kwargs)),
+    #     },
+    #     sock_dir=__opts__['sock_dir'],
+    #     transport=__opts__['transport'],
+    # )
+
+    # try:
+    #     data = _query('server/create', method='POST', data=_urlencode(kwargs))
+    #     if int(data.get('status', '200')) >= 300:
+    #         log.error('Error creating {0} on Vultr\n\n'
+    #             'Vultr API returned {1}\n'.format(vm_['name'], data))
+    #         log.error('Status 412 may mean that you are requesting an\n'
+    #                   'invalid location, image, or size.')
+
+    #         __utils__['cloud.fire_event'](
+    #             'event',
+    #             'instance request failed',
+    #             'salt/cloud/{0}/requesting/failed'.format(vm_['name']),
+    #             args={'kwargs': kwargs},
+    #             sock_dir=__opts__['sock_dir'],
+    #             transport=__opts__['transport'],
+    #         )
+    #         return False
+    # except Exception as exc:
+    #     log.error(
+    #         'Error creating {0} on Vultr\n\n'
+    #         'The following exception was thrown when trying to '
+    #         'run the initial deployment: \n{1}'.format(
+    #             vm_['name'], str(exc)
+    #         ),
+    #         # Show the traceback if the debug logging level is enabled
+    #         exc_info_on_loglevel=logging.DEBUG
+    #     )
+    #     __utils__['cloud.fire_event'](
+    #         'event',
+    #         'instance request failed',
+    #         'salt/cloud/{0}/requesting/failed'.format(vm_['name']),
+    #         args={'kwargs': kwargs},
+    #         sock_dir=__opts__['sock_dir'],
+    #         transport=__opts__['transport'],
+    #     )
+    #     return False
+
+    # def wait_for_hostname():
+    #     '''
+    #     Wait for the IP address to become available
+    #     '''
+    #     data = show_instance(vm_['name'], call='action')
+    #     main_ip = str(data.get('main_ip', '0'))
+    #     if main_ip.startswith('0'):
+    #         time.sleep(3)
+    #         return False
+    #     return data['main_ip']
+
+    # def wait_for_default_password():
+    #     '''
+    #     Wait for the IP address to become available
+    #     '''
+    #     data = show_instance(vm_['name'], call='action')
+    #     # print("Waiting for default password")
+    #     # pprint.pprint(data)
+    #     if str(data.get('default_password', '')) == '':
+    #         time.sleep(1)
+    #         return False
+    #     return data['default_password']
+
+    # def wait_for_status():
+    #     '''
+    #     Wait for the IP address to become available
+    #     '''
+    #     data = show_instance(vm_['name'], call='action')
+    #     # print("Waiting for status normal")
+    #     # pprint.pprint(data)
+    #     if str(data.get('status', '')) != 'active':
+    #         time.sleep(1)
+    #         return False
+    #     return data['default_password']
+
+    # def wait_for_server_state():
+    #     '''
+    #     Wait for the IP address to become available
+    #     '''
+    #     data = show_instance(vm_['name'], call='action')
+    #     # print("Waiting for server state ok")
+    #     # pprint.pprint(data)
+    #     if str(data.get('server_state', '')) != 'ok':
+    #         time.sleep(1)
+    #         return False
+    #     return data['default_password']
+
+    # vm_['ssh_host'] = __utils__['cloud.wait_for_fun'](
+    #     wait_for_hostname,
+    #     timeout=config.get_cloud_config_value(
+    #         'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
+    # )
+    # vm_['password'] = __utils__['cloud.wait_for_fun'](
+    #     wait_for_default_password,
+    #     timeout=config.get_cloud_config_value(
+    #         'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
+    # )
+    # __utils__['cloud.wait_for_fun'](
+    #     wait_for_status,
+    #     timeout=config.get_cloud_config_value(
+    #         'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
+    # )
+    # __utils__['cloud.wait_for_fun'](
+    #     wait_for_server_state,
+    #     timeout=config.get_cloud_config_value(
+    #         'wait_for_fun_timeout', vm_, __opts__, default=15 * 60),
+    # )
+
+    # __opts__['hard_timeout'] = config.get_cloud_config_value(
+    #     'hard_timeout',
+    #     get_configured_provider(),
+    #     __opts__,
+    #     search_global=False,
+    #     default=None,
+    # )
+
+    # # Bootstrap
+    # ret = __utils__['cloud.bootstrap'](vm_, __opts__)
+
+    # ret.update(show_instance(vm_['name'], call='action'))
+
+    # log.info('Created Cloud VM \'{0[name]}\''.format(vm_))
+    # log.debug(
+    #     '\'{0[name]}\' VM creation details:\n{1}'.format(
+    #     vm_, pprint.pformat(data)
+    #         )
+    # )
+
+    # __utils__['cloud.fire_event'](
+    #     'event',
+    #     'created instance',
+    #     'salt/cloud/{0}/created'.format(vm_['name']),
+    #     args=__utils__['cloud.filter_event']('created', vm_, ['name', 'profile', 'provider', 'driver']),
+    #     sock_dir=__opts__['sock_dir'],
+    #     transport=__opts__['transport']
+    # )
+
+    # return ret
+
+def __get_connection():
+
+    conn = salt.utils.boto3.get_connection('lightsail')
+
+def _query(params):
     '''
     Perform a query directly against the Vultr REST API
     '''
@@ -486,7 +508,11 @@ def _query(path, method='GET', data=None, params=None, header_dict=None, decode=
         # import pdb
         # pdb.set_trace()
 
-        q_ret = aws_utils.query(product='lightsail',
+        conn = __get_connection()
+        print("Connection:", conn)
+        q_ret = aws_utils.query(
+                    params=params,
+                    product='lightsail',
                     location=aws_utils.DEFAULT_LOCATION,
                     provider=get_provider(),
                     opts=__opts__,
